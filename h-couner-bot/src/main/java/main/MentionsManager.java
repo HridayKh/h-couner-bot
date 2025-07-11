58dbfe8528fc48e2f7e05e1c13ab2e19a14bf6b6
HridayDev <hridaykh1234@gmail.com>
2025-07-11 19:19:16 +0530

    a

--- h-couner-bot/src/main/java/main/MentionsManager.java
+++ h-couner-bot/src/main/java/main/MentionsManager.java
@@ -72,6 +72,7 @@ public class MentionsManager {
 						+ message.getString("body") + ")");
 				fullnamesToMarkAsRead.add(messageFullname);
 				Main.processedMessageFullnames.add(messageFullname);
+				continue;
 			}
 
 			String author = message.getString("author");
